79f9a76a9c0b36589ed778aa6aba78e14a6959ef
sena <mymachines0@gmail.com>
2024-10-31 08:33:22 -0700

    css change

--- netflix-clone/src/Componenets/Rows/Row/Row.js
+++ netflix-clone/src/Componenets/Rows/Row/Row.js
@@ -62,7 +62,7 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
                     />
                 ))}
             </div>
-            <div style={{ padding: '40px' }}>
+            <div style={{ padding: '20px' }}>
                 {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
             </div>
         </div>
